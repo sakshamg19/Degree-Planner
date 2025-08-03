6d0af99e2e0bd23f9ff224f4384181c840a2036d
Saksham Gurung <sakshamgrg19@gmail.com>
2025-08-03 22:42:18 +0530

    Implemented JWT authentication with register, login, refresh, and protected route

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from api import views
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
     path('courses/', views.course_list, name='course-list'),
     path('courses/<int:pk>/', views.course_detail, name='course-detail'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #for login token
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), #for refresh token
+    path('register/', views.register, name='register'),
+    path('protected/', views.protected_view, name='protected-view'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
@@ -65,3 +66,9 @@ def register(request):
     except KeyError:
         return Response({'error': 'Username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def protected_view(request):
+    return Response({
+        'message': f'Welcome, {request.user.username}! You are authenticated.'
+    })
