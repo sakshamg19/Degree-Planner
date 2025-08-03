da190665867690a6e97ba076bf8a865a822da3d4
Saksham Gurung <sakshamgrg19@gmail.com>
2025-08-03 21:17:26 +0530

    Switched DB to postgres

--- courseplanner/settings.py
+++ courseplanner/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'courseplanner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'courseplanner_db',
+        'USER': 'courseplanner_user',
+        'PASSWORD': 'planner301',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
